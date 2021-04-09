53a72077496da5ffc519bf64fe149af43b5ee1f8
docty89 <docty89@gmail.com>
2021-04-09 17:50:06 +0000

    bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   
-   resources :blogs
+   resources :books
  
    get 'top' => 'homes#top'
    get 'books' => 'books#index'
+   
+   post 'books' => 'books#create'
   
-  
-  
+   get 'books/:id' => 'books#show'
+   get 'books/:id/edit' => 'books#edit'
+   patch 'books/:id' => 'books#update'
+   delete 'books/:id' => 'books#destroy'
+   
   
    root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
